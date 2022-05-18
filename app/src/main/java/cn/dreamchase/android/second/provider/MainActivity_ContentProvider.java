d8b74e1d096ce06c61990e14514a85bd5e5b2902
test_endless@126.com <test_endless@126.com>
2022-05-18 12:51:11 +0800

    android 开发

---
+++ app/src/main/java/cn/dreamchase/android/second/provider/MainActivity_ContentProvider.java
@@ -0,0 +1,45 @@
+package cn.dreamchase.android.second.provider;
+
+
+import android.database.Cursor;
+import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.util.Log;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import cn.dreamchase.android.second.R;
+
+public class MainActivity_ContentProvider extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.i("MainActivity", "onCreate Thread : " + android.os.Process.myTid());
+
+
+    }
+
+    private void readContacts() {
+        Cursor cursor = null;
+
+        try {
+            cursor = getContentResolver().query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null, null, null, null);
+
+            while (cursor.moveToNext()) {
+                int columnIndex = cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME);
+                int columnIndex1 = cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER);
+                String displayName = cursor.getString(columnIndex);
+                String number = cursor.getString(columnIndex1);
+            }
+        }catch (Exception e) {
+            e.printStackTrace();
+        }finally {
+            if (cursor != null) {
+                cursor.close();
+            }
+        }
+    }
+
+}
